492a00a052c7bbefee59ae0d66da2ea19c3c7d45
hexburn <LanLura1212@protonmail.com>
2024-08-08 22:01:19 +0200

    init

---
+++ API/Components/Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace API.Components.Pages
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
