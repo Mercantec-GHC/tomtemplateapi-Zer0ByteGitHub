f84e3853170d5f4f498b63a362193da28b1f3442
hexburn <LanLura1212@protonmail.com>
2024-08-08 22:13:24 +0200

    variables names text on home page

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,20 @@
 using API.Components;
 
-var builder = WebApplication.CreateBuilder(args);
+var WebBuilder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
+WebBuilder.Services
+    .AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddHttpClient();
+WebBuilder.Services.AddHttpClient();
 
-var app = builder.Build();
+var app = WebBuilder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change t  his for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492a00a052c7bbefee59ae0d66da2ea19c3c7d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
